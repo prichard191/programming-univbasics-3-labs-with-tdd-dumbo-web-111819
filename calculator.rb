1e09a9f7ce6c66fc9f14a4c68c812e24fdd42f22
Patrick Richard <prichard191@gmail.com>
2019-10-30 23:31:33 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
-# Add your variables here
+# Add your variabes here
+
+first_number.Integer = 7
+second_number.Float = 8.4
+product = first_number*second_number
+difference = first_number-second_number
+quotient = first_number/second_number
